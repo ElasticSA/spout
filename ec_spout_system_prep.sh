f50339573258caa5cbfbb8317291c6e088da9d8e
Thorben Jändling <thorbenj@users.noreply.github.com>
2020-10-28 10:06:27 +0100

    yq version bump

--- ec_spout_system_prep.sh
+++ ec_spout_system_prep.sh
@@ -7,7 +7,7 @@ fi
 
 cd $SCRIPTDIR
 
-YQ_PKG_URL="https://github.com/mikefarah/yq/releases/download/3.4.0/yq_linux_amd64"
+YQ_PKG_URL="https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64"
 
 install_with_apt()
 {
