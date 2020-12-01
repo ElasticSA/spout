e1746bbfaa9086b16b778643e4bf907d67df6523
Thorben Jändling <thorbenj@users.noreply.github.com>
2020-12-01 09:34:13 +0100

    rta ttp script improvements

--- rta_ttp.py
+++ rta_ttp.py
@@ -19,37 +19,35 @@ def main(argv):
     stack_version = default_stack_version
     ttp_list = ['ALL']
     dry_run = False
+    list_modules = None
     
     try:
-        opts, args = getopt.getopt(argv,"hds:t:",["stack=","ttp="])
+        opts, args = getopt.getopt(argv,"hdl:s:t:",["stack=","ttp=","list="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print_help()
-            exit()
+            return 0
         elif opt in '-d':
             dry_run = True
         elif opt in ('-s', '--stack'):
             stack_version = arg
         elif opt in ('-t', '--ttp'):
             ttp_list = arg.split(',')
+        elif opt in ('-l', '--list'):
+            list_modules = arg
 
-    el_dr_zip = f"detection-rules-{stack_version}.zip"
-    el_dr_url = f"https://github.com/elastic/detection-rules/archive/v{stack_version}.zip"
+    rta = get_rta_modules(stack_version)
 
-    if not os.path.isfile(f"{mydir}/{el_dr_zip}"):
-        print(f"Downloading: {el_dr_url}")
-        download_url(el_dr_url, f"{mydir}/{el_dr_zip}")
-    
-    if not os.path.isdir(f"{mydir}/detection-rules-{stack_version}"):
-        print(f"Extracting zip: {mydir}/{el_dr_zip}")
-        with zipfile.ZipFile(f"{mydir}/{el_dr_zip}", 'r') as this_zip:
-                this_zip.extractall(mydir)
+    if list_modules != None:
+        if list_modules == '':
+            list_modules = rta.common.CURRENT_OS
 
-    sys.path.insert(1, f"{mydir}/detection-rules-{stack_version}")
-    rta = importlib.import_module('rta')
+        for mod in rta.get_ttp_names(list_modules):
+            print(mod)
+        return 0
 
     if ttp_list[0] == 'ALL':
         ttp_list = rta.get_ttp_names(rta.common.CURRENT_OS)
@@ -64,14 +62,38 @@ def main(argv):
             if not dry_run:
                 ttp.main()
         except Exception as Ex:
-            print(f"RAT TTP FAILED: ", Ex)           
+            print(f"RTA TTP FAILED: ", Ex)
         print(f"RTA TTP Finished: {ttp_name}")
     
     return 0
 
+def get_rta_modules(sver):
+    # Not all stack point releases have a detection rules release
+    # So we'll just always get x.y.0
+    _sver = sver.split('.')
+    sver = f"{_sver[0]}.{_sver[1]}.0"
+
+    el_dr_id = f"detection-rules-{sver}"
+    el_dr_url = f"https://github.com/elastic/detection-rules/archive/v{sver}.zip"
+
+    if not os.path.isfile(f"{mydir}/{el_dr_id}.zip"):
+        print(f"Downloading: {el_dr_url}")
+        download_url(el_dr_url, f"{mydir}/{el_dr_id}.zip")
+
+    if not os.path.isdir(f"{mydir}/{el_dr_id}"):
+        print(f"Extracting zip: {mydir}/{el_dr_id}.zip")
+        with zipfile.ZipFile(f"{mydir}/{el_dr_id}.zip", 'r') as this_zip:
+                this_zip.extractall(mydir)
+
+    sys.path.insert(1, f"{mydir}/{el_dr_id}")
+    return importlib.import_module('rta')
+
 def print_help():
-    print('test.py -s <stack_version> -t <ttp,name,list>')
+    print('rta_ttp.py -s <stack_version> -t <ttp,name,list> -d')
+    print('rta_ttp.py -l <os_name>')
+    print(f"Defaults/example:")
     print(f"--stack={default_stack_version} (stack_version)\n--ttp=ALL (ttp list)")
+    print(f"-list='' (list for current os")
 
 def download_url(url, save_path, chunk_size=4096):
     r = requests.get(url, stream=True)
